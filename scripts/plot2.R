4c7c04d316fa4bb39951ecc15ac8c1b9bfe858a6
Ayush Gupta <ayush.gupta@healthkart.com>
2016-06-26 11:07:22 +0530

    Update plot2.R

--- scripts/plot2.R
+++ scripts/plot2.R
@@ -1,3 +1,3 @@
-png(filename='plots/plot2.png',width=480,height=480,units='px')
+png(filename='plot2.png',width=480,height=480,units='px')
 with(valid_exData, plot(time,Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)" ))
 dev.off()
